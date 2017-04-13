9bcd0023b2950b89e13be82bd3559e95c7aa53b0
Anndy Dong <anndy.dong@sap.com>
2017-04-13 16:38:26 +0800

    disable dual screen when it unavailable

--- srui/WebContent/js/util.js
+++ srui/WebContent/js/util.js
@@ -5,14 +5,20 @@ var util = {
     badgeIdLength: 10,
     nodataTimeout: 5000,
     openSecondWindow: function (url, winName) {
-		var orgwin = window.open('', winName);
-		if (orgwin) {
-			orgwin.close();
-		}
-		// open new window
-		orgwin = window.open(url, winName, 'fullscreen=1, toolbar=0, menubar=0, status=0, screenX=' + 
-				 window.screen.availWidth + ', left=' + window.screen.availWidth);
-		return orgwin;
+  		var orgwin = window.open('', winName);
+  		if (orgwin) {
+  			orgwin.close();
+  		}
+  		// open new window
+  		orgwin = window.open(url, winName, 'fullscreen=1, toolbar=0, menubar=0, status=0, screenX=' + 
+  				 window.screen.availWidth + ', left=' + window.screen.availWidth);
+  		setTimeout(function() {
+    	  if (orgwin.screenLeft !== window.screen.availWidth) {
+    	    orgwin.close();
+    	    return null;
+    	  }
+  		},1);
+  		return orgwin;
     }
 };
 
