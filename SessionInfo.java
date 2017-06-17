8440ea66a3982520a2e939efe5dbdf861288b33e
Anndy Dong <anndy.dong@sap.com>
2017-06-17 22:10:06 +0800

    chargeCC to string type @BKEND

---
+++ SessionInfo.java
@@ -0,0 +1,41 @@
+package com.sap.it.sr.dto;
+
+public class SessionInfo {
+	private String role;
+	private String currentUser;
+	private String userFullName;
+	private String error;
+	private String chargeCC;
+	
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getCurrentUser() {
+		return currentUser;
+	}
+	public void setCurrentUser(String currentUser) {
+		this.currentUser = currentUser;
+	}
+	public String getUserFullName() {
+		return userFullName;
+	}
+	public void setUserFullName(String userFullName) {
+		this.userFullName = userFullName;
+	}
+	public String getError() {
+		return error;
+	}
+	public void setError(String error) {
+		this.error = error;
+	}
+    public String getChargeCC() {
+        return chargeCC;
+    }
+    public void setChargeCC(String chargeCC) {
+        this.chargeCC = chargeCC;
+    }
+	
+}
---
+++ User.java
@@ -0,0 +1,109 @@
+package com.sap.it.sr.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User implements Serializable {
+    private static final long serialVersionUID = 185706989928100701L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userName;
+
+    private String fullName;
+    
+    private boolean status;
+    
+    private String password;
+    
+    private String role;
+    
+    private String pickLocation;
+    
+    private String remark;
+    
+    private String chargeCC;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+	    if (userName != null) {
+	        userName = userName.toUpperCase();
+        }
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public String getFullName() {
+		return fullName;
+	}
+
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+
+	public boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getPickLocation() {
+		return pickLocation;
+	}
+
+	public void setPickLocation(String pickLocation) {
+		this.pickLocation = pickLocation;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getChargeCC() {
+		return chargeCC;
+	}
+
+	public void setChargeCC(String chargeCC) {
+		this.chargeCC = chargeCC;
+	}
+
+}
