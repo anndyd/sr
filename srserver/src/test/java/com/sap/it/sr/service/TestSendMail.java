5004ac13f274ad2baa649219d6dc0bf44d43bafe
Anndy Dong <anndy.dong@sap.com>
2017-12-05 21:19:55 +0800

    try send mail via ssl

--- srserver/src/test/java/com/sap/it/sr/service/TestSendMail.java
+++ srserver/src/test/java/com/sap/it/sr/service/TestSendMail.java
@@ -1,8 +1,23 @@
 package com.sap.it.sr.service;
 
 import java.sql.Timestamp;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 
 import org.junit.Test;
 
@@ -12,7 +27,7 @@ import com.sap.it.sr.entity.PickupData;
 
 public class TestSendMail {
 	private final SendMail sm = new SendMail();
-	@Test
+	//@Test
 	public void TestSendEquipmentReadyNotificationEmail() {
 		PickupData pd = new PickupData();
 				ItemInfo itm = new ItemInfo();
@@ -51,5 +66,45 @@ public class TestSendMail {
 
 		pd.setPickupTime(currentTime);
 		sm.sendPickedEmail(pd, Arrays.asList(""), Arrays.asList("I063098"));
+	}
+	
+	@Test
+	public void SendMailSSL() {
+        String sender = "Tester@exchange.sap.corp";
+        String receiver = "%s@exchange.sap.corp";
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "mail.sap.corp");
+        props.put("mail.smtp.socketFactory.port", "587");
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", "true");
+//        Session session = Session.getInstance(props, null);
+        Session session = Session.getDefaultInstance(props,
+                new javax.mail.Authenticator() {
+                                    @Override
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication("i063098@sap.com","Asdfjkl0");
+                    }
+                });
+        try {
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(sender));
+      
+            List<InternetAddress> toAddr = new ArrayList<>();
+            toAddr.add(new InternetAddress(String.format(Locale.ENGLISH, receiver, "i063098")));
+            message.setRecipients(Message.RecipientType.TO, toAddr.toArray(new Address[toAddr.size()]));
+            message.setSubject("Test SSL mail.");
+            
+            String content = "This is a test mail.";
+            Multipart parts = new MimeMultipart();
+            MimeBodyPart part = new MimeBodyPart();
+            part.setText(content, "GBK");
+            part.setContent(content, "text/html;charset=GBK");
+            parts.addBodyPart(part);
+            message.setContent(parts);
+            Transport.send(message);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
 	}
 }
